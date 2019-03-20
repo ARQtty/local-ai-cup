513986f888bf441c38b7ab3c1c4f6591ff7e1ce2
ARQtty <22442872+ARQtty@users.noreply.github.com>
2019-03-20 19:03:16 +0300

    Изменена метрика расстояния на количество клеток, переведены комментарии

--- game.js
+++ game.js
@@ -1,24 +1,22 @@
-// Game field is rectangle with size N
+// Размер игрового поля
 var sizeY = field.length;
 var sizeX = field[0].length;
+var gameProcess;
+
+var DEBUG = false;
 var visualDistance = 4;
 
-var gameProcess;
-var DEBUG = false;
-
-// Units service code
+// Настройка юнитов
 var idCounter = 0;
 var units = [];
 function Unit(player, posX, posY){
+	// Класс Юнит
 	this.owner = player;
 	this.position = {x: posX, y: posY};
-	this.killed = 0;
-	this.eaten  = 0;
 	this.id = generateId();
 }
 function generateId(){
 	idCounter++;
-	// console.log(idCounter);
 	return "u" + idCounter;
 }
 (function exampleUnitsGeneration(){
@@ -32,6 +30,7 @@ function generateId(){
 	units.push(un1, un2, un3, un4, un5, un6);
 })();
 function getPlayerUnits(playerName){
+	// Возвращает всех юнитов указанного игрока
 	let units = [];
 	for (let i=0; i<units.length; i++)
 		if (units[i].owner == playerName)
@@ -40,28 +39,30 @@ function getPlayerUnits(playerName){
 }
 
 
-// Players service code
+// Настройки игроков
 var currPlayer = "ARQ"; // Who starts the game
-var players = [new BotFarmer("ARQ", "#32cd32"), new BotRandom("Guest", "#005000")]; // [bot1, bot2....]
-// Distributing units to players
+var players = [new BotFarmer("FarmerBot", "#32cd32"), new BotRandom("RandomBot", "#005000")]; // [bot1, bot2....]
+// Распределение юнитов по игрокам
 players[0].units = units.slice(0, 3);
 players[1].units = units.slice(3, 6);
 
 
 
-// Game service code
+// Методы хост-процесса игры
 var steps = 0;
 var iterationInterval, foodSpawnChance, foodOnStart, warFog, cellSize;
 function startGame(){
-	// Game main loop
+	// Хост-процесс игры
 	loadSettings();
 
-	console.log("On top - "+players[0].strategyName);
-	console.log("On bottom - "+players[1].strategyName);
+	console.log("Сверху - "+players[0].name);
+	console.log("Снизу  - "+players[1].name);
 	
 	for (let i=0; i<foodOnStart; i++) spawnFood();
 	drawGameState();
 	
+	// Раз в iterationInterval миллисекунд запускается функция, вызывающая метод
+	// .step() у игрока, деающего ход
 	gameProcess = setInterval(function(){
 		for (let p=0; p<players.length; p++){
 			if (players[p].name == currPlayer){
@@ -72,87 +73,98 @@ function startGame(){
 			spawnFood();
 
 		drawGameState();
-		currPlayer = (currPlayer == "ARQ")? "Guest" : "ARQ";
+		nextPlayerTurn();
 		steps++;
 	}, iterationInterval);
 }
 function relaunchGame(){
+	// Перезапускает игру по нажатию кнопки на панели настроек
 	clearInterval(gameProcess);
 	loadSettings();
 	startGame();
 }
 function loadSettings(){
+	// Загружает данные из панели настроек
 	iterationInterval = document.getElementById("iterationTime").value;
 	foodSpawnChance = document.getElementById("foodSpawnChance").value;
 	foodOnStart = document.getElementById("foodOnStart").value;
 	cellSize = document.getElementById("cellSize").value;
 	warFog = document.getElementById("warFog").checked;
 }
+// unused
 function countPlayersScore(){
 	for (let p=0; p<players.length; p++){
 		let score = players[p].killed + players[p].eaten;
 		if (score > 5){
-			console.warn("Player "+players[p].name+" wins by score!");
+			console.warn("Игрок "+players[p].name+" победил, достигнув цели по счёту!");
 			return false;
 		}
 	}
 	return true;
 }
-
+function nextPlayerTurn(){
+	// Обновляет очередь игроков ходить
+	for (let p=0; p<players.length; p++){
+		if ((players[p].name == currPlayer) && (p != players.length-1))
+			currPlayer = players[p+1].name;
+		else if ((players[p].name == currPlayer) && (p == players.length-1))
+			currPlayer = players[0].name;
+		else
+			console.error("Невозможно построить очередь игроков. Нет игрока после "+currPlayer);
+	}
+}
 
 
 // Private
-// Methods for change game data throgh the game
+// Внутриигровые методы изменения состояния игры
 function getUnitById(id){
 	for (let i=0; i<units.length; i++)
 		if (units[i].id == id)
 			return units[i];
-	console.error("Cant find unit with id " + id);
+	console.error("Невозможно найти юнита с id "+id);
 }
 function fedUnit(unit_id){
-	for (let i=0; i<units.length; i++)
-		if (units[i].id ==	unit_id)
-			units[i].eaten++;
-
+	// Засчитывает съеденную еду
 	for (let p=0; p<players.length; p++)
 		if (players[p].name == currPlayer)
 			players[p].eaten++;
 
-	console.log("Unit "+unit_id+" fed");
+	console.log("Юнит "+unit_id+" съел еду");
 }
 function countKillFor(unit_id){
-	for (let i=0; i<units.length; i++)
-		if (units[i].id == unit_id)
-			units[i].killed++;
-
+	// Засчитывает убийство боту, чей юнит совершил убийство
 	for (let p=0; p<players.length; p++)
 		if (players[p].name == currPlayer)
 			players[p].kills++;
 }
 function deleteDeadUnit(unit_id){
+	// Актуализирует информацию о живых юнитах. Запускается после смерти юнита на поле
 	for (let i=0; i<units.length; i++){
 		if (units[i].id == unit_id){
 			units = units.slice(0, i).concat(units.slice(i+1, units.length));
+			// Обновляет массив units каждого игрока
 			for (let p=0; p<players.length; p++){
 				try{
 					players[p].updateAliveUnits();
 				}catch(e){
-					console.error("Cant find "+players[p].name+"'s method updateAliveUnits()");
+					console.error("У бота "+players[p].name+"отсутствует метод updateAliveUnits()");
 				}
 			}
 		}
 	}
 }
 function spawnFood(){
+	// Кладёт еду в случайную клетку поля. Если невозможно нарандомить пустую клетку, запускается ещё раз
 	let x = Math.round(Math.random()*(sizeX-1));
 	let y = Math.round(Math.random()*(sizeY-1));
-	// console.log(x, y)
+
 	if (isFree(x, y) && !isUnit(x, y))
 		field[y][x] = "F";
 	else
 		spawnFood();
 }
 function isUnit(posX, posY){
+	// Проверяет наличие юнита в клетке. Если он есть, возвращает объект с этим юнитом
 	for (let i=0; i<units.length; i++)
 		if ((units[i].position.x == posX) && (units[i].position.y == posY))
 			return units[i];
@@ -160,12 +172,11 @@ function isUnit(posX, posY){
 
 
 // Public
-// Game statements are change with basic actions of units
-// Bots are interrating with game by these methods
+// Для изменения ботами состояния игры
 function moveUp(unit_id){
 	let unit = getUnitById(unit_id);
 	if (unit.owner != currPlayer){
-		console.error("Cant move unit "+unit_id+" course its not "+currPlayer+"'s step");
+		console.error("Невозможно подвинуть юнита "+unit_id+". Сейчас не ход "+currPlayer);
 	}else{
 		if (isFree(unit.position.x, unit.position.y-1)){
 			for (let i=0; i<units.length; i++)
@@ -174,14 +185,14 @@ function moveUp(unit_id){
 				}
 		}else{
 			if (DEBUG)
-				console.log("Cant move unit "+unit_id+" course of cell is not empty");
+				console.log("Невозможно подвинуть юнита "+unit_id+". Клетка ("+unit.position.x+","+(unit.position.y-1)+") занята");
 		}
 	}
 }
 function moveDown(unit_id){
 	let unit = getUnitById(unit_id);
 	if (unit.owner != currPlayer){
-		console.error("Cant move unit "+unit_id+" course its not "+currPlayer+"'s step");
+		console.error("Невозможно подвинуть юнита "+unit_id+". Сейчас не ход "+currPlayer);
 	}else{
 		if (isFree(unit.position.x, unit.position.y+1)){
 			for (let i=0; i<units.length; i++)
@@ -189,14 +200,14 @@ function moveDown(unit_id){
 					units[i].position.y++;
 		}else{
 			if (DEBUG)
-				console.log("Cant move unit "+unit_id+" course of cell is not empty");
+				console.log("Невозможно подвинуть юнита "+unit_id+". Клетка ("+unit.position.x+","+(unit.position.y+1)+") занята");
 		}
 	}
 }
 function moveLeft(unit_id){
 	let unit = getUnitById(unit_id);
 	if (unit.owner != currPlayer){
-		console.error("Cant move unit "+unit_id+" course its not "+currPlayer+"'s step");
+		console.error("Невозможно подвинуть юнита "+unit_id+". Сейчас не ход "+currPlayer);
 	}else{
 		if (isFree(unit.position.x-1, unit.position.y)){
 			for (let i=0; i<units.length; i++)
@@ -204,14 +215,14 @@ function moveLeft(unit_id){
 					units[i].position.x--;
 		}else{
 			if (DEBUG)
-				console.log("Cant move unit "+unit_id+" course of cell is not empty");
+				console.log("Невозможно подвинуть юнита "+unit_id+". Клетка ("+(unit.position.x-1)+","+unit.position.y+") занята");
 		}
 	}
 }
 function moveRight(unit_id){
 	let unit = getUnitById(unit_id);
 	if (unit.owner != currPlayer){
-		console.error("Cant move unit "+unit_id+" course its not "+currPlayer+"'s step");
+		console.error("Невозможно подвинуть юнита "+unit_id+". Сейчас не ход "+currPlayer);
 	}else{
 		if (isFree(unit.position.x+1, unit.position.y)){
 			for (let i=0; i<units.length; i++)
@@ -219,23 +230,22 @@ function moveRight(unit_id){
 					units[i].position.x++;
 		}else{
 			if (DEBUG)
-				console.log("Cant move unit "+unit_id+" course of cell is not empty");
-		}
+				console.log("Невозможно подвинуть юнита "+unit_id+". Клетка ("+(unit.position.x+1)+","+unit.position.y+") занята");
 	}
 }
 function eatFood(unit_id, foodPos) {
 	let unit = getUnitById(unit_id);
 	if (unit.owner != currPlayer){
-		console.error("Cant eat foot with unit "+unit_id+" course its not "+currPlayer+"'s step");
+		console.error("Невозможно съесть еду юнитом "+unit_id+". Сейчас не ход "+currPlayer);
 	}else{
 		if (isFood(foodPos.x, foodPos.y) && dist(unit.position, foodPos) == 1){
 			field[foodPos.x][foodPos.y] = 0;
 			fedUnit(unit_id);
 		}else{
 			if (dist(unit.position, foodPos) >1){
-				console.error("Cant eat foot with unit "+unit_id+" course food at ("+foodPos.x+","+foodPos.y+") is too far (dist "+dist(unit.position, foodPos));
+				console.error("Слишком большая дистанция для поедания юнитом "+unit_id+" еды в клетке ("+foodPos.x+","+foodPos.y+"). Дистанция="+dist(unit.position, foodPos));
 			}else
-				console.error("Cant eat foot with unit "+unit_id+" course there is no food at ("+foodPos.x+","+foodPos.y+")");
+				console.error("Невозможно съесть юнитом "+unit_id+". Нет еды в клетке ("+foodPos.x+","+foodPos.y+")");
 		}
 	}
 }
@@ -243,38 +253,36 @@ function attackUnit(unit_id, enemy_unit_id) {
 	let unit = getUnitById(unit_id);
 	let enemy= getUnitById(enemy_unit_id);
 	if (unit.owner != currPlayer){
-		console.error("Cant attack with unit "+unit_id+" course its not "+currPlayer+"'s step");
+		console.error("Невозможно атаковать юнитом "+unit_id+" юнита "+enemy_unit_id+". Сейчас не ход "+currPlayer);
 	}else{
 		if (dist(unit.position, enemy.position) <= 1){
 			countKillFor(unit_id);
-			console.log("Unit "+unit_id+" killed unit "+enemy_unit_id);
+			console.log("Юнит "+unit_id+" убил юнита "+enemy_unit_id);
 			deleteDeadUnit(enemy_unit_id);
 		}else{
-			console.error("Distance to attack with unit "+unit_id+" is too much to attack");
+			console.error("Слишком большая дистанция для атаки юнитом "+unit_id+" юнита "+enemy_unit_id);
 		}
 	}
 }
 
 
 // Public
-// Bots can borrow information about world by these methods
+// Для получения ботами информации о мире
 function dist(pos1, pos2) {
-	return Math.sqrt(Math.pow(pos1.x - pos2.x, 2) + 
-					 Math.pow(pos1.y - pos2.y, 2));
+	// Расстояние от от одной клетки до другой (в клетках пути)
+	return Math.abs(pos1.x - pos2.x) + Math.abs(pos1.y - pos2.y);
 }
 function inField(posX, posY) {
-	// Used in isFree(), isFood() and inVisible() methods to avoid indexing to negative cells
+	// Используется в isFree(), isFood() и inVisible() для избежания выхода за границы массива
 	
 	if ((posX >= 0) && (posY >= 0) && (posX < sizeX) && (posY < sizeY))
 		return true;
 }
 function isFree(posX, posY) {
-	// Checks availability of position with coordinates (x, y) to move into
-	// Warning! Doesn't check for placing enemy unit in this cell
+	// Проверяет доступность клетки для шага в неё. Не проверяет наличие юнита в ней!
 	
 	if (inField(posX, posY) && (field[posX][posY] == 0)){
 		for (let i=0; i<units.length; i++){
-			// console.log("unit with pos ",units[i].position);
 			if ((units[i].position.x == posX) && (units[i].position.y == posY))
 				return false;
 		}
@@ -282,14 +290,14 @@ function isFree(posX, posY) {
 	}
 }
 function isFood(posX, posY) {
-	// Checks the cell with coordinates (posX, posY) for food
+	// Проверяет клетку с указанными координатами на наличие еды
 	
 	// F is food
 	if (inField(posX, posY) && (field[posX][posY] == "F"))
 		return true;
 }
 function inVisible(unit_id) {
-	// Returns list of all objects in neighborhood (units and food)
+	// Возвращает список объектов в окрестности Фон Неймана
 	
 	// Food object example 
 	// {
@@ -316,12 +324,12 @@ function inVisible(unit_id) {
 	// ########## 
 	let unit = getUnitById(unit_id);
 	if (unit.owner != currPlayer){
-		console.error("Cant get vision with unit "+unit_id+" course its not "+currPlayer+"'s step");
+		console.error("Невозможно получить видимое "+unit_id+". Сейчас не ход "+currPlayer);
 	}else{
 		let visibleObjs = [];
 		for (let i=-visualDistance; i<=visualDistance; i++){
 			for (let j=-visualDistance; j<=visualDistance; j++){
-				// console.log("In field", i, j, ((j == 0) && (i == 0)));
+				// TODO заменить на BFS
 				if ((j == 0) && (i == 0) || (Math.abs(j) + Math.abs(i) > 3)){
 					continue;
 				}
